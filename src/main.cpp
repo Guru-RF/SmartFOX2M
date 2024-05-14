049f90184a632bf56b8f65eee68201dfaff6e237
Joeri Van Dooren <ure@moreorless.be>
2024-05-14 02:06:46 +0200

    fixed

--- src/main.cpp
+++ src/main.cpp
@@ -12,32 +12,40 @@ void setup() {
   pinMode(11, OUTPUT);
   // reset the SI chip
   digitalWrite(3, LOW);
-  delay(2000);
+  digitalWrite(15, LOW);
   Serial.println("SmartFOX booted !");
   // start the SI chip
   digitalWrite(3, HIGH);
-  delay(1);
 
   Wire1.setSDA(26);
   Wire1.setSCL(27);
+  delay(10);
 
   bool i2c_found;
 
   // Start serial and initialize the Si5351
-  i2c_found = si5351.init(SI5351_CRYSTAL_LOAD_0PF, 0, -220);
+  i2c_found = si5351.init(SI5351_CRYSTAL_LOAD_0PF, 250000000, -220);
   if (!i2c_found) {
     Serial.println("Device not found on I2C bus!");
   }
   si5351.reset();
-  delay(200);
   si5351.set_ms_source(SI5351_CLK0, SI5351_PLLB);
+  si5351.set_freq(14477500000ULL, SI5351_CLK0);
   si5351.set_vcxo(PLLB_FREQ, 31); // pull weight
-  // si5351.set_freq_manual(14545000000ULL, PLLB_FREQ, SI5351_CLK0);
-  si5351.set_freq_manual(14477500000ULL, PLLB_FREQ, SI5351_CLK0);
+  si5351.drive_strength(SI5351_CLK0, SI5351_DRIVE_2MA);
+  si5351.output_enable(SI5351_CLK0, 1);
+  si5351.set_clock_pwr(SI5351_CLK1, 0);
+  si5351.set_clock_pwr(SI5351_CLK2, 0);
+  si5351.set_clock_pwr(SI5351_CLK3, 0);
+  si5351.set_clock_pwr(SI5351_CLK4, 0);
+  si5351.set_clock_pwr(SI5351_CLK5, 0);
+  si5351.set_clock_pwr(SI5351_CLK6, 0);
+  si5351.set_clock_pwr(SI5351_CLK7, 0);
 }
 
 void loop() {
   si5351.output_enable(SI5351_CLK0, 1);
+
   for (int i = 0; i < 5; i++) {
     Serial.println("Ring");
     tone(15, 425, 1000);
